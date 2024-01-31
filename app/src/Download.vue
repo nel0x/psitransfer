95194b814258b7a1d14177cfb9d4cc9fa41c5e38
Nicko Hristov <87320197+nel0x@users.noreply.github.com>
2024-01-31 22:14:01 +0100

    smaller gce logo + remove upload btn from download page

--- app/src/Download.vue
+++ app/src/Download.vue
@@ -1,8 +1,5 @@
 <template lang="pug">
   .download-app
-    a.btn.btn-sm.btn-info.btn-new-session(@click='newSession()', :title='$root.lang.newUpload')
-      icon.fa-fw(name="cloud-upload-alt")
-      span.hidden-xs  {{ $root.lang.newUpload }}
     .alert.alert-danger(v-show="error")
       strong
         icon.fa-fw(name="exclamation-triangle")
